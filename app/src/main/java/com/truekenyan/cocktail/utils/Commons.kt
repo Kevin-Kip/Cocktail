40ce234dc80ec0ad36dbf1ba1f0bc34a355061af
Kevin-Kip <masterfork5@gmail.com>
2018-12-04 15:00:40 +0300

    Changed from statis to object

--- app/src/main/java/com/truekenyan/cocktail/utils/Commons.kt
+++ app/src/main/java/com/truekenyan/cocktail/utils/Commons.kt
@@ -1,14 +1,13 @@
 package com.truekenyan.cocktail.utils
 
-class Commons {
-    companion object {
-        const val FIRST_TIME_LAUNCH = "cocktail-first-time-launch"
-        const val HAS_LAUNCHED = "has-launched"
-        const val DRINK_ID = "drink-id"
-        const val URL_ALCOHOLIC = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-        const val URL_NON_ALCOHOLIC = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-        const val SEARCH_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
-        const val COCKTAIL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-        const val MORE_COCKTAILS = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
-    }
+object Commons {
+    const val FIRST_TIME_LAUNCH = "cocktail-first-time-launch"
+    const val HAS_LAUNCHED = "has-launched"
+    const val DRINK_ID = "drink-id"
+    const val URL_ALCOHOLIC = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+    const val URL_NON_ALCOHOLIC = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+    const val SEARCH_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+    const val COCKTAIL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+    const val MORE_COCKTAILS = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+    const val URL_RANDOM = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
 }
