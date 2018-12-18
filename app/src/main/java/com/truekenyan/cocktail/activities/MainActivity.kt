8fe8a5d9dd869928b404a449eeaea3fc9b10fba3
Kevin-Kip <masterfork5@gmail.com>
2018-12-18 16:56:30 +0300

    Fixed listener

--- app/src/main/java/com/truekenyan/cocktail/activities/MainActivity.kt
+++ app/src/main/java/com/truekenyan/cocktail/activities/MainActivity.kt
@@ -3,15 +3,33 @@ package com.truekenyan.cocktail.activities
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
 import com.truekenyan.cocktail.R
+import com.truekenyan.cocktail.fragments.FragmentHome
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var linearLayout: LinearLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        linearLayout = findViewById(R.id.message)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
+                val fragment = FragmentHome()
+                val manager = supportFragmentManager
+                manager.beginTransaction()
+                        .add(R.id.message, fragment)
+                        .commit()
             }
             R.id.navigation_search -> {
                 return@OnNavigationItemSelectedListener true
@@ -19,14 +37,14 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_favorites -> {
                 return@OnNavigationItemSelectedListener true
             }
+            else -> {
+                val fragment = FragmentHome()
+                val manager = supportFragmentManager
+                manager.beginTransaction()
+                        .add(R.id.message, fragment)
+                        .commit()
+            }
         }
         false
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
