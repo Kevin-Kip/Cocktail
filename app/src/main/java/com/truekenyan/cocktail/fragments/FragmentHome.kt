ca2711d9d38d9f6748d8f17ce5e3ad9c51b7797e
Kevin-Kip <masterfork5@gmail.com>
2018-12-18 16:56:42 +0300

    Created fragment

--- app/src/main/java/com/truekenyan/cocktail/fragments/FragmentHome.kt
+++ app/src/main/java/com/truekenyan/cocktail/fragments/FragmentHome.kt
@@ -1,4 +1,16 @@
-package com.truekenyan.cocktail
+package com.truekenyan.cocktail.fragments
 
-class FragmentHome {
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.truekenyan.cocktail.R
+
+class FragmentHome : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        return rootView
+    }
 }
