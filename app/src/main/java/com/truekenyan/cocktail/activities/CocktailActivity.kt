8d30f1146743cfd9ac256be47bcac086d7c07115
Kevin-Kip <masterfork5@gmail.com>
2018-12-24 18:52:21 +0300

    Code to fetch data from api

--- app/src/main/java/com/truekenyan/cocktail/activities/CocktailActivity.kt
+++ app/src/main/java/com/truekenyan/cocktail/activities/CocktailActivity.kt
@@ -1,11 +1,10 @@
 package com.truekenyan.cocktail.activities
 
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.support.v4.util.LruCache
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.widget.ImageView
@@ -14,21 +13,22 @@ import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import com.squareup.picasso.Picasso
 import com.truekenyan.cocktail.R
 import com.truekenyan.cocktail.adapters.IngredientsAdapter
 import com.truekenyan.cocktail.models.CocktailModel
 import com.truekenyan.cocktail.models.Ingredient
 import com.truekenyan.cocktail.utils.Commons
+import kotlinx.android.synthetic.main.activity_cock_tail.*
 import org.json.JSONObject
 
 class CocktailActivity : AppCompatActivity() {
 
     private var ingredients = mutableListOf<Ingredient>()
-    private var drinkId: String? = null
+    private var drinkId: String? = "13060" //TODO use fetched ID
     private lateinit var i: Intent
     private lateinit var cockTail: CocktailModel
 
@@ -37,22 +37,13 @@ class CocktailActivity : AppCompatActivity() {
     private lateinit var methodText: TextView
     private lateinit var ingredientsRecycler: RecyclerView
 
-    private val requestQueue: RequestQueue = Volley.newRequestQueue(applicationContext)
-    private val imageCache: ImageLoader.ImageCache = object : ImageLoader.ImageCache{
-        private val cache: LruCache<String, Bitmap> = LruCache(30)
-        override fun getBitmap(url: String?): Bitmap {
-            return cache.get(url as String) as Bitmap
-        }
-
-        override fun putBitmap(url: String?, bitmap: Bitmap?) {
-            cache.put(url as String, bitmap as Bitmap)
-        }
-    }
-    private val imageLoader: ImageLoader = ImageLoader(requestQueue, imageCache)
+    private var requestQueue: RequestQueue? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cock_tail)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         initViews()
 
@@ -61,11 +52,14 @@ class CocktailActivity : AppCompatActivity() {
             drinkId = i.getStringExtra(Commons.DRINK_ID)
         }
 
+        requestQueue = Volley.newRequestQueue(this@CocktailActivity)
+
         ingredientsAdapter = IngredientsAdapter(applicationContext,ingredients)
         ingredientsRecycler.apply {
             adapter = ingredientsAdapter
             hasFixedSize()
             itemAnimator = DefaultItemAnimator()
+            layoutManager = LinearLayoutManager(this@CocktailActivity)
         }
     }
 
@@ -79,15 +73,15 @@ class CocktailActivity : AppCompatActivity() {
             Commons.COCKTAIL+drinkId,
             JSONObject(),
             Response.Listener {
-                val drinkObject: JSONObject = (it.getJSONArray("drinks"))[0] as JSONObject
+                val drinkObject: JSONObject = (it.getJSONArray(Commons.DRINKS))[0] as JSONObject
                 cockTail = Gson().fromJson(drinkObject.toString(),CocktailModel::class.java)
-
+                ingredients.clear()
                 for (i in 1..15){
                     val ingredient = drinkObject.get("strIngredient$i") as String?
                     val measure = drinkObject.get("strMeasure$i") as String?
 
-                    if (!ingredient.isNullOrEmpty()){
-                        if (measure.isNullOrEmpty()){
+                    if (!ingredient.equals("")){
+                        if (measure.equals("")){
                             ingredients.add(Ingredient(ingredient,""))
                         } else {
                             ingredients.add(Ingredient(ingredient, measure))
@@ -95,7 +89,10 @@ class CocktailActivity : AppCompatActivity() {
                     }
                 }
 
+                method.text = cockTail.strInstructions
                 ingredientsAdapter.setIngredients(ingredients)
+                Picasso.get().load(cockTail.strDrinkThumb).into(cocktail_image)
+                collapsing_toolbar.title = cockTail.strDrink
             },
             Response.ErrorListener {
                 Toast.makeText(applicationContext, "Unable to fetch cocktail", Toast.LENGTH_SHORT).show()
@@ -104,6 +101,6 @@ class CocktailActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        requestQueue.add(jsonObjectRequest)
+        requestQueue!!.add(jsonObjectRequest)
     }
 }
