3ab3cafb98a03db44836698178c154e5c88a3fdd
Kevin-Kip <masterfork5@gmail.com>
2018-12-24 23:22:44 +0300

    Minor additions

--- app/src/main/java/com/truekenyan/cocktail/utils/Commons.kt
+++ app/src/main/java/com/truekenyan/cocktail/utils/Commons.kt
@@ -9,6 +9,7 @@ object Commons {
     const val URL_NON_ALCOHOLIC = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
     const val SEARCH_URL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
     const val COCKTAIL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-    const val MORE_COCKTAILS = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
     const val URL_RANDOM = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    const val ALCOHOLIC = "Alcoholic"
+    const val NON_ALCOHOLIC = "Non Alcoholic"
 }
