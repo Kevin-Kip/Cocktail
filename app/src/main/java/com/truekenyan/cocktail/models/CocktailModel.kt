32286316dcabcdc5a980b3176d9c089ea6cdc231
Kevin-Kip <masterfork5@gmail.com>
2018-12-26 18:30:35 +0300

    Exteended parcelable

--- app/src/main/java/com/truekenyan/cocktail/models/CocktailModel.kt
+++ app/src/main/java/com/truekenyan/cocktail/models/CocktailModel.kt
@@ -1,6 +1,7 @@
 package com.truekenyan.cocktail.models
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
 data class CocktailModel(var idDrink: String?,
                     var strDrinkThumb: String?,
@@ -9,4 +10,37 @@ data class CocktailModel(var idDrink: String?,
                     var strAlcoholic: String?,
                     var strCategory: String?,
                     var strGlass: String?
-): Serializable
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest!!.writeString(idDrink)
+        dest.writeString(strDrinkThumb)
+        dest.writeString(strDrink)
+        dest.writeString(strInstructions)
+        dest.writeString(strAlcoholic)
+        dest.writeString(strCategory)
+        dest.writeString(strGlass)
+    }
+
+    override fun describeContents(): Int {
+        return Parcelable.CONTENTS_FILE_DESCRIPTOR
+    }
+
+    companion object CREATOR : Parcelable.Creator<CocktailModel> {
+        override fun createFromParcel(parcel: Parcel): CocktailModel {
+            return CocktailModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<CocktailModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
