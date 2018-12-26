136f80c9c8f15bedbd3fdd947cc4200cbc725b98
Kevin-Kip <masterfork5@gmail.com>
2018-12-27 00:58:29 +0300

    Renamed file

---
+++ app/src/main/java/com/truekenyan/cocktail/callbacks/CocktailDao.kt
@@ -0,0 +1,21 @@
+package com.truekenyan.cocktail.callbacks
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import com.truekenyan.cocktail.models.Fav
+import com.truekenyan.cocktail.utils.Commons
+
+@Dao
+interface CocktailDao {
+
+    @Query("SELECT * FROM ${Commons.DRINKS}")
+    fun getFavs(): MutableList<Fav?>
+
+    @Insert
+    fun addToFavs(drink: Fav?)
+
+    @Delete
+    fun removeFromFavs(drink: Fav?)
+}
