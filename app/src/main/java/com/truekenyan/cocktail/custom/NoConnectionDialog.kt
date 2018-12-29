6b12219039cf06e9fc2ebe1056df61e59258e480
Kevin-Kip <masterfork5@gmail.com>
2018-12-29 21:01:45 +0300

    Created dialog class

--- app/src/main/java/com/truekenyan/cocktail/custom/NoConnectionDialog.kt
+++ app/src/main/java/com/truekenyan/cocktail/custom/NoConnectionDialog.kt
@@ -1,4 +1,19 @@
 package com.truekenyan.cocktail.custom
 
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.truekenyan.cocktail.R
+
 class NoConnectionDialog {
+    private fun create(activity: AppCompatActivity): AlertDialog {
+        val parentView = activity.findViewById<ViewGroup>(R.id.container)
+        val dialogView = LayoutInflater.from(activity).inflate(R.layout.dialog_no_connection, parentView, false)
+
+        val builder = AlertDialog.Builder(activity).apply {
+            setView(dialogView)
+        }
+        return builder.create()
+    }
 }
