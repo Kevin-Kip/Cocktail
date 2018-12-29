49ea93560a410160eca0f004d437f0b70f76ad5e
Kevin-Kip <masterfork5@gmail.com>
2018-12-29 21:02:29 +0300

    Moved sort functionality to function and added options menu

--- app/src/main/java/com/truekenyan/cocktail/fragments/FragmentHome.kt
+++ app/src/main/java/com/truekenyan/cocktail/fragments/FragmentHome.kt
@@ -21,9 +21,9 @@ import com.truekenyan.cocktail.adapters.CocktailAdapter
 import com.truekenyan.cocktail.callbacks.Callbacks
 import com.truekenyan.cocktail.models.CocktailModel
 import com.truekenyan.cocktail.utils.Commons
+import com.truekenyan.cocktail.utils.PrefManager
 import org.json.JSONArray
 import org.json.JSONObject
-import java.util.*
 
 class FragmentHome : Fragment() {
 
@@ -33,6 +33,7 @@ class FragmentHome : Fragment() {
     private var cocktails = mutableListOf<CocktailModel>()
     private var requestQueue: RequestQueue? = null
     private var listener: Callbacks? = null
+    private var prefManager: PrefManager? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_home, container, false)
@@ -40,16 +41,10 @@ class FragmentHome : Fragment() {
         homeList = rootView.findViewById(R.id.home_list)
         cocktailAdapter = CocktailAdapter(context!!, cocktails, true)
         requestQueue = Volley.newRequestQueue(context)
+        prefManager = PrefManager(context!!.applicationContext)
 
         setHasOptionsMenu(true)
-
-        if((Random().nextInt() % 2) == 0){
-            fetchDrinks(Commons.URL_ALCOHOLIC)
-            listener!!.onTitleFound(Commons.ALCOHOLIC)
-        } else {
-            fetchDrinks(Commons.URL_NON_ALCOHOLIC)
-            listener!!.onTitleFound(Commons.NON_ALCOHOLIC)
-        }
+        fetchDrinks()
 
         homeList.apply {
             adapter = cocktailAdapter
@@ -60,9 +55,55 @@ class FragmentHome : Fragment() {
         return rootView
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        super.onPrepareOptionsMenu(menu)
+        val randomItem = menu?.findItem(R.id.random)
+        val ascItem = menu?.findItem(R.id.ascending)
+        val descItem = menu?.findItem(R.id.descending)
+        when {
+            prefManager!!.getOrder() == Commons.RANDOM -> randomItem!!.isChecked = true
+            prefManager!!.getOrder() == Commons.ASCENDING -> ascItem!!.isChecked = true
+            prefManager!!.getOrder() == Commons.DESCENDING -> descItem!!.isChecked = true
+        }
+
+        val alcoholic = menu?.findItem(R.id.alcoholic)
+        val nonAlcoholic = menu?.findItem(R.id.non_alcoholic)
+        when (prefManager!!.isAlcoholic()){
+            true -> alcoholic!!.isChecked = true
+            else -> nonAlcoholic!!.isChecked = true
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater!!.inflate(R.menu.main_options, menu)
+        inflater!!.inflate(R.menu.home_options, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId){
+            R.id.alcoholic -> {
+                prefManager!!.setAlcoholic(true)
+                fetchDrinks()
+            }
+            R.id.non_alcoholic -> {
+                prefManager!!.setAlcoholic(false)
+                fetchDrinks()
+            }
+            R.id.ascending -> {
+                prefManager!!.setOrder(Commons.ASCENDING)
+                sortList()
+            }
+            R.id.descending -> {
+                prefManager!!.setOrder(Commons.DESCENDING)
+                sortList()
+            }
+            R.id.random -> {
+                prefManager!!.setOrder(Commons.RANDOM)
+                sortList()
+            }
+        }
+        activity!!.invalidateOptionsMenu()
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onAttach(context: Context?) {
@@ -70,7 +111,24 @@ class FragmentHome : Fragment() {
         listener = context as Callbacks
     }
 
-    private fun fetchDrinks(drinkUrl: String?){
+    private fun getTitle(){
+        when {
+            prefManager!!.isAlcoholic() -> listener!!.onTitleFound(Commons.ALCOHOLIC)
+            else -> listener!!.onTitleFound(Commons.NON_ALCOHOLIC)
+        }
+    }
+
+    private fun fetchDrinks(){
+        val drinkUrl: String? = when(prefManager!!.isAlcoholic()) {
+            true -> Commons.URL_ALCOHOLIC
+            else -> Commons.URL_NON_ALCOHOLIC
+        }
+
+        progressBar.visibility = View.VISIBLE
+        homeList.visibility = View.GONE
+        cocktails.clear()
+        getTitle()
+
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET,
                 drinkUrl,
                 JSONObject(),
@@ -85,8 +143,7 @@ class FragmentHome : Fragment() {
                             cocktails.add(cockTail)
                         }
                     }
-                    cocktails = cocktails.shuffled() as MutableList<CocktailModel>
-                    cocktailAdapter.setItems(cocktails)
+                    sortList()
                 },
                 Response.ErrorListener {
                     Toast.makeText(context, "Oooops. Unable to fetch drinks", Toast.LENGTH_SHORT).show()
@@ -94,5 +151,14 @@ class FragmentHome : Fragment() {
                 })
 
         requestQueue!!.add(jsonObjectRequest)
+    }
+
+    private fun sortList(){
+        when {
+            prefManager!!.getOrder() == Commons.ASCENDING -> cocktails.sortBy { it.strDrink }
+            prefManager!!.getOrder() == Commons.DESCENDING -> cocktails.sortByDescending {it.strDrink}
+            else -> cocktails = cocktails.shuffled() as MutableList<CocktailModel>
+        }
+        cocktailAdapter.setItems(cocktails)
     }
 }
