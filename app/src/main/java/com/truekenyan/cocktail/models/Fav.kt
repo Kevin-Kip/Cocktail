30bf24fbc77fdda8f6dbd130e5cfa756d5251e97
Kevin-Kip <masterfork5@gmail.com>
2019-02-13 12:33:57 +0300

    Added initializations

--- app/src/main/java/com/truekenyan/cocktail/models/Fav.kt
+++ app/src/main/java/com/truekenyan/cocktail/models/Fav.kt
@@ -6,6 +6,6 @@ import com.truekenyan.cocktail.utils.Commons
 
 @Entity(tableName = Commons.DRINKS)
 data class Fav(@PrimaryKey(autoGenerate = true) var favId: Int?,
-               var drinkId: String?,
-               var drinkName: String?,
-               var drinkPhoto: String?)
+               var drinkId: String? = "",
+               var drinkName: String? = "",
+               var drinkPhoto: String? = "")
